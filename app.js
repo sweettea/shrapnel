cccd610ab3baa2d777031382bc410a78c44c8ad6
Sweet Tea Dorminy <sweettea@mit.edu>
2013-03-28 12:03:30 -0400

    fix port

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.configure('production', function(){
 // Routes
 
 app.post('/messages', routes.incoming_handler);
-
-app.listen(3000, function(){
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
